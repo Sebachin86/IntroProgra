ab11231012748ce05349d4546b2695c90785917a
Sebachin86 <141078671+Sebachin86@users.noreply.github.com>
2023-08-03 00:00:15 +0000

    Ejercicio 10

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 #Python
 #Ejercicio 1
-print('Hola Mundo')
+#print('Hola Mundo')
 #Ejercicio 2
-Saludo = '¡Hola Mundo!'
-print(Saludo)
+#Saludo = '¡Hola Mundo!'
+#print(Saludo)
+#Ejercicio 3
+#print('Hola, ¿Cuál es tu nombre?')
+#Nombre = input()
+#print('Hola, ' + Nombre + '!')
+#Ejercicio 4
+#print(((3+2)/(2*5))**2)
+#Ejercicio 5
+#print('Ingrese el Número de horas trabajadas')
+#HorasTrabajadas = float(input())
+#print('Ingrese el Costo por hora del lugar donde trabaja')
+#CostoPorHora = float(input())
+#Pago = str(HorasTrabajadas*CostoPorHora)
+#print('El pago que le corresponde es de $' + Pago + ' Pesos.')
+#Ejercicio 6
+#print('Ingrese un número entero positivo')
+#N = int(input())
+#Resultado = ((N*(N+1))/2)
+#print(Resultado)
+#Ejercicio 7
+#print('Indique su peso (En kg)')
+#Peso = input()
+#print('Ahora, Indique su Estatura (En M)')
+#Estatura = input()
+#MasaCorporal = round(float(Peso)/float(Estatura)**2,2)
+#print('Tu Índice de Masa Corporal es', MasaCorporal)
+#Ejercicio 8
+#print('Escribe dos números enteros')
+#N = int(input())
+#M = int(input())
+#C = str(N/M) 
+#print('El Resultado es: ', C)
+#R = str(N%M)
+#print('Residuo: ', R)
+#Ejercicio 9
+#print('A continuación digite la cantidad a invertir')
+#Inversion = float(input())
+#print('Ahora, el interés anual')
+#Interes = round(float(input()))
+#print('Y por último, los años')
+#Años = int(input())
+#CapitalObtenido = str(round(Inversion*(Interes/100+1)**Años,2))
+#print('Capital obtenido en la Inversión:', CapitalObtenido)
+#Ejercicio 10
